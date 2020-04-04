48776012ce812a16b1cea112778d6ec7c3f2b8fb
Jiaxuan Wang <nathanwang000@gmail.com>
2020-04-04 03:42:07 -0400

    added improvisation tune

--- composition/simple.py
+++ composition/simple.py
@@ -9,15 +9,17 @@ parser = argparse.ArgumentParser(description="simple composition tool try")
 parser.add_argument('-t', type=str, nargs='+', default=['c', 'e', 'g'])
 args = parser.parse_args()
 
-def main(ts, tempo='4=140', time_signature='4/4'):
+def main(lines, tempo='4=140', time_signature='4/4'):
     '''
-    ts is time series to play
+    lines: list of time serieses to play
     '''
-    staff = "\score{  \n\\new PianoStaff << \n"
-    staff += "  \\new Staff { \\clef treble \\tempo %s \\time %s " \
-             % (tempo, time_signature) +\
-             " ".join(ts) + "}\n"
-    staff += ">>\n \layout {} \midi{} }\n"
+    body = "\score{\n \\new PianoStaff << \n"
+    for ts in lines:
+        body += "  \\new Staff { \\clef treble \\tempo %s \\time %s "\
+                % (tempo, time_signature)
+        body += " ".join(ts)
+        body += "}\n"
+    body += ">>\n \layout {} \midi{} }\n"
 
     title = """\\version "2.18.2"
     \header {
@@ -26,7 +28,7 @@ def main(ts, tempo='4=140', time_signature='4/4'):
     tagline = "Copyright: MIT license"
     }"""
 
-    print(title + staff)
+    print(title + body)
 
 def number2note(n, scale, use_pitch=True):
     note, pitch = n % len(scale), n // len(scale) if use_pitch else 0
@@ -41,15 +43,98 @@ def swing(chord1, chord2=None):
     chord2 = chord2 if chord2 else chord1
     return r"\tuplet 3/2 %s8~ %s %s " % (chord1, chord1, chord2)
 
-if __name__ == '__main__':
-    # todo develop a small language for myself to write simple chord
-    cmaj = [chr(ord("a") + i) for i in range(7)]
-    cmaj = cmaj[2:] + cmaj[:2]
+def abs_note2number(note):
+    '''
+    calculate the absolute number from note to number
+    ex: "c" -> 0
+    ex: "fis''" -> 12 * 2 + 6 = 30
+    ex: "c," -> -12
+    '''
+    notes = ['c', 'd', 'e', 'f', 'g', 'a', 'b']
+    degrees = [0, 2, 4, 5, 7, 9, 11]
+    n = degrees[notes.index(note[0])]
+    c = ""
+    for p in range(1, len(note)):
+        c = c + note[p]
+        if c == 'is':
+            n += 1
+            c = ""
+        elif c == 'es':
+            n -= 1
+            c = ""
+        elif c == "'":
+            n += 12
+            c = ""            
+        elif c == ',':
+            n -= 12
+            c = ""
+    assert c == "", "have unparsed note {}".format(c)
+    return n
+            
+def abs_number2note(n, scale=None):
+    '''
+    the inverse of abs_note2number
+    but this is still ambiguous: e.g., fes = e = disis,
+    need to be given scale to determine what is
+    '''
+    if scale is None:
+        scale = ['c', 'cis', 'd', 'ees', 'e', 'f', 'fis', 'g', 'gis', 'a', 'bes', 'b']
+    note, pitch = scale[n % 12], n // 12
+    return note + ("," if pitch < 0 else "'") * abs(pitch)
 
-    chords5 = [swing(chord(cmaj, [i,i+4])) for i in range(8)]
-    chords6 = [swing(chord(cmaj, [i,i+5])) for i in range(8)]
-    finish = [swing(chord(cmaj, [i,i+5]),
-                    chord(cmaj, [i,i+4])) + r"\fermata" for i in range(8)]
+def build_scale(root, mode='Ionian'):
+    '''
+    build scale from the root
+    example usage:
+    scale = build_scale('c', 'dorian')
+    main(scale)
+    '''
+    modes = ['ionian', 'dorian', 'phrygian', 'lydian', 'mixolydian',
+             'aeolian', 'locrian']
+    # convert mode to number
+    if type(mode) is str:
+        mode = dict((a, b) for a, b in zip(modes, range(len(modes))))[mode.lower()]
+
+    diffs = [2, 2, 1, 2, 2, 2, 1] # diff between nodes in 'ionian'
+    diffs = np.cumsum([0] + diffs[mode:] + diffs[:mode])
+    n = abs_note2number(root)
+    return [abs_number2note(n+diff) for diff in diffs]
+
+################## rhythm notations #############
+def add_rhythm(notes, rhythm_pattern=None, unit=16):
+    '''
+    unit: the smallest granularity
+    rhythm_pattern: [2, 3] means 2 continuous unit followed by a 3 continuous unit
+    '''
+    if rhythm_pattern is None:
+        rhythm_pattern = [1] * len(notes)
+        
+    assert len(rhythm_pattern) == len(notes), 'rhythm must have the same length as notes'
+    
+    def simplify(rythm, note, unit=16):
+        '''
+        now is just a dumb slur; should simplfy later
+        '''
+        notes = ['{}{}'.format(note, unit) for _ in range(rythm)]
+        if len(notes) > 1:
+            notes[0] = notes[0] + '('
+            notes[-1] = notes[-1] + ')'        
+        return '~ '.join(notes)
+    
+    return [simplify(r, n, unit=unit) for r, n in zip(rhythm_pattern, notes)]
+
+################## Applications #################
+def tweleve_bar_blues(scale):
+    '''
+    12 bar blues 
+    example usage:
+    scale = build_scale('c', 'dorian')
+    tweleve_bar_blues(scale)
+    '''
+    chords5 = [swing(chord(scale, [i,i+4])) for i in range(8)]
+    chords6 = [swing(chord(scale, [i,i+5])) for i in range(8)]
+    finish = [swing(chord(scale, [i,i+5]),
+                    chord(scale, [i,i+4])) + r"\fermata" for i in range(8)]
     # 12 bar blues
     ts = [chords5[1], chords6[1], chords5[1], chords6[1],
           chords5[1], chords6[1], chords5[1], chords6[1],
@@ -65,3 +150,19 @@ if __name__ == '__main__':
           chords5[1], chords6[1], chords5[1], finish[1]]
     
     main(ts, tempo='4=160')
+
+if __name__ == '__main__':
+    # for improvisation: https://www.youtube.com/watch?v=o7dGlZAMKi0
+    scale = build_scale("c", 'dorian')
+    rhythm = [1,1,1,5]
+    notes = [scale[i-1] for i in [1, 3, 5, 6]]
+    bass = add_rhythm(notes, rhythm, unit=8)
+
+    scale = build_scale("c'", 'dorian')
+    up = [scale[i-1] for i in list(range(1,8))]
+    down = [scale[i-1] for i in list(range(8,1,-1))]
+    upper = add_rhythm(up + ['r'] + down + ['r'], unit=8)
+    
+    lines = [upper, bass * 5]
+    main(lines, tempo='4=100')
+    
--- composition/ts2music.py
+++ composition/ts2music.py
@@ -14,7 +14,6 @@ def normalize(ts, n_notes):
     return a np array of size of len(ts), quantized as n_notes
     round down
     '''
-    print(ts)
     return ((np.array(ts) - min(ts)) / (max(ts) - min(ts)) * n_notes).astype(int)
 
 def number2note(n, n_notes):
@@ -25,7 +24,8 @@ def number2note(n, n_notes):
 def transpose(ts, notes, degree):
     return [notes[(notes.index(note) - degree) % len(notes)] for note in ts]
 
-def main(args, span=4, time_signature=4, key='c', bass_degree=3, bass_ts=None):
+def main(args, span=4, time_signature='3/4', tempo='4=210', time_unit=4,
+         key='c', bass_degree=3, bass_ts=None):
     '''
     span: how many 7 notes span
     '''
@@ -34,7 +34,6 @@ def main(args, span=4, time_signature=4, key='c', bass_degree=3, bass_ts=None):
     n_notes = span * 7
     n2note = partial(number2note, n_notes=n_notes)
     notes = list(map(n2note, np.arange(n_notes)))
-    print(notes)
     
     ts = normalize(args.t, n_notes-1) # -1 for note using the n_notes note b/c 0 based
     ts = list(map(n2note, ts))
@@ -48,11 +47,10 @@ def main(args, span=4, time_signature=4, key='c', bass_degree=3, bass_ts=None):
         logging.warning(bass)        
 
     staff = "\score{\n\\new PianoStaff << \n"
-    staff += "  \\new Staff { \\tempo 4=210 \\time 3/4 " + " ".join(map(lambda x: x + str(time_signature),
-                                              ts)) + "}\n"
-    # staff += "  \\new Staff {\clef bass "+" ".join(map(lambda x: x + str(time_signature),
-    #                                                   bass)) + "}\n"
-    staff += "  \\new Staff {"+" ".join(map(lambda x: x + str(time_signature),
+    staff += "  \\new Staff { \\clef treble \\tempo %s \\time %s " \
+             % (tempo, time_signature) +\
+             " ".join(map(lambda x: x + str(time_unit), ts)) + "}\n"
+    staff += "  \\new Staff {"+" ".join(map(lambda x: x + str(time_unit),
                                                       bass)) + "}\n"  
     
     
@@ -84,15 +82,15 @@ if __name__ == '__main__':
         return ret[:length]
 
     def stock(ticker, length, interval='1d'):
-        prices = yf.download(ticker, interval=interval)
+        prices = yf.download(ticker, interval=interval, progress=False)
         return np.array(prices['Close'])[-length:]
     
-    # args.t = np.sin(ts)
+    args.t = np.sin(ts)
     # args.t = np.exp(ts)
     # args.t = decimal(np.pi, len(ts))
     # args.t = decimal(np.e, len(ts))
     # args.t = decimal(np.sqrt(2), len(ts))
-    args.t = stock('MSFT', len(ts), interval='1d')
+    # args.t = stock('MSFT', len(ts), interval='1d')
     # args.t = stock('GOOGL', len(ts)) # variations F
     # bass_ts = stock('GOOGL', len(ts))
 
